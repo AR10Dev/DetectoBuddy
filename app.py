922dcc3bc674be60b2e350078e1581fd22fe7c71
Avaab Razzaq <40202832+AR10Dev@users.noreply.github.com>
2024-07-05 23:03:11 +0000

    chore: Download YOLO model during app initialization

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import customtkinter as ctk
 from tabs import Tabs
+import os
+import requests
+from constants import MODEL_PATH
+
 
 class AutoDetectorApp(ctk.CTk):
     def __init__(self):
         super().__init__()
+
+        # Download YOLO model
+        self.download_yolo_model()
 
         # Set window title and disable resizing
         self.title("AutoDetector v1.0.0")
@@ -17,14 +24,25 @@ class AutoDetectorApp(ctk.CTk):
         self.initialize_image_tab_elements()
         self.initialize_video_tab_elements()
 
+    def download_yolo_model(self):
+        if not os.path.exists(MODEL_PATH):
+            os.makedirs(MODEL_PATH, exist_ok=True)
+            model_url = "https://github.com/ultralytics/assets/releases/download/v8.2.0/yolov8n.pt"
+            response = requests.get(model_url)
+            with open(os.path.join(MODEL_PATH, "yolov8n.pt"), "wb") as model_file:
+                model_file.write(response.content)
+
     def initialize_image_tab_elements(self):
-        self.image_path_header = ctk.CTkLabel(master=self.tab_view.tab("Image"), text="", font=("Arial", 20))
+        self.image_path_header = ctk.CTkLabel(
+            master=self.tab_view.tab("Image"), text="", font=("Arial", 20))
         self.image_path_header.place(relx=0.5, rely=0.6, anchor="center")
 
-        self.image_error_path_header = ctk.CTkLabel(master=self.tab_view.tab("Image"), text="", font=("Arial", 20), text_color="red")
+        self.image_error_path_header = ctk.CTkLabel(master=self.tab_view.tab(
+            "Image"), text="", font=("Arial", 20), text_color="red")
         self.image_error_path_header.place(relx=0.5, rely=0.6, anchor="center")
 
-        self.image_path = ctk.CTkLabel(master=self.tab_view.tab("Image"), text="", font=("Arial", 15))
+        self.image_path = ctk.CTkLabel(master=self.tab_view.tab(
+            "Image"), text="", font=("Arial", 15))
         self.image_path.place(relx=0.5, rely=0.65, anchor="center")
 
         self.image_detect_button = ctk.CTkButton(master=self.tab_view.tab("Image"), text="Detect now", font=("Arial", 30),
@@ -32,19 +50,22 @@ class AutoDetectorApp(ctk.CTk):
         self.image_detect_button.place_forget()
 
     def initialize_video_tab_elements(self):
-        self.video_path_header = ctk.CTkLabel(master=self.tab_view.tab("Video"), text="", font=("Arial", 20))
+        self.video_path_header = ctk.CTkLabel(
+            master=self.tab_view.tab("Video"), text="", font=("Arial", 20))
         self.video_path_header.place(relx=0.5, rely=0.6, anchor="center")
 
-        self.video_error_path_header = ctk.CTkLabel(master=self.tab_view.tab("Video"), text="", font=("Arial", 20), text_color="red")
+        self.video_error_path_header = ctk.CTkLabel(master=self.tab_view.tab(
+            "Video"), text="", font=("Arial", 20), text_color="red")
         self.video_error_path_header.place(relx=0.5, rely=0.6, anchor="center")
 
-        self.video_path = ctk.CTkLabel(master=self.tab_view.tab("Video"), text="", font=("Arial", 15))
+        self.video_path = ctk.CTkLabel(master=self.tab_view.tab(
+            "Video"), text="", font=("Arial", 15))
         self.video_path.place(relx=0.5, rely=0.65, anchor="center")
 
         self.video_detect_button = ctk.CTkButton(master=self.tab_view.tab("Video"), text="Detect now", font=("Arial", 30),
                                                  command=lambda: self.video_detection(), width=150, height=18, corner_radius=10)
         self.video_detect_button.place_forget()
-    
+
     def image_detection(self):
         from detection import image_detection
         image_detection(self)
@@ -52,7 +73,7 @@ class AutoDetectorApp(ctk.CTk):
     def video_detection(self):
         from detection import video_detection
         video_detection(self)
-    
+
     def webcam_detection(self):
         from detection import webcam_detection
         webcam_detection(self)
